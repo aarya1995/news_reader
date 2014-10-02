8976294cf51e3a9c504aa9fa9437982eb8c44113
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2014-10-01 21:10:45 -0400

    configure postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140916010759) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "feed_entries", force: true do |t|
     t.string   "name"
     t.text     "summary"
